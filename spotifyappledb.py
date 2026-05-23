923da69ad66c60c21e45801a64734c69985acb93
mediatajdemo-design <mediatajdemo@gmail.com>
2026-05-23 23:24:39 +0500

    Update spotifyappledb.py

--- spotifyappledb.py
+++ spotifyappledb.py
@@ -1,11 +1,13 @@
 from supabase import create_client
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+print("SUPABASE_URL =", SUPABASE_URL)
+print("SUPABASE_KEY =", SUPABASE_KEY)
 
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
